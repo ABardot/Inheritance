28daaa4e7ac4c08c56483cd62b838a317d678706
ABardot <aabardot5@gmail.com>
2018-02-05 11:47:23 -0500

    Added Rectangle and Shapes class

---
+++ Inheritance/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿namespace Inheritance
+{
+    public class Rectangle : Shape
+    {
+        private double area;
+
+        public Rectangle(double area)
+        {
+            this.area = area;
+        }
+
+        public double Area
+        {
+            get { return this.area; }
+            set {this.area = value; }
+        }
+    }
+}
---
+++ Inheritance/Shape.cs
@@ -0,0 +1,28 @@
+﻿namespace Inheritance
+{
+    public class Shape
+    {
+        // Fields
+        private int sides;
+
+        // Properties
+        public int Sides
+        {
+            get { return this.sides; }
+            set { this.sides = value; }
+        }
+
+        // Constructor 
+        public Shape()
+        {
+            // Default
+        }
+
+        public Shape(int sides)
+        {
+            this.sides = sides;
+        }
+
+        // Methods
+    }
+}
